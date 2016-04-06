b442c77d424792df77bcfaaa6d87f2d77b67ed9e
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2016-04-06 12:26:53 +0100

    use setup tools

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "piccolo2-common",
+    version = "0.1",
+    namespace_packages = ['piccolo2'],
+    packages = find_packages(),
+)
