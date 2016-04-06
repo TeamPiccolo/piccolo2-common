f60143003be0f2313017c084760590b24467456d
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2016-04-06 16:12:05 +0100

    update README and add metadata to setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ setup(
     version = "0.1",
     namespace_packages = ['piccolo2'],
     packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = "Magnus Hagdorn, Alasdair MacArthur, Iain Robinson",
+    description = "Part of the piccolo2 system. This package provides common modules",
+    license = "GPL",
+    url = "https://bitbucket.org/uoepiccolo/piccolo2-common",
 )
