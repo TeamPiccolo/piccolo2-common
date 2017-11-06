642ff7345ca560a316b84396e2fb20392e04ba02
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2017-11-06 12:44:34 +0000

    get version from hg tag

--- setup.py
+++ setup.py
@@ -19,10 +19,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = "piccolo2-common",
-    version = "0.1",
     namespace_packages = ['piccolo2'],
     packages = find_packages(),
-
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     # metadata for upload to PyPI
     author = "Magnus Hagdorn, Alasdair MacArthur, Iain Robinson",
     description = "Part of the piccolo2 system. This package provides common modules",
